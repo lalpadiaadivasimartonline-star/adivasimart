f2b2da1046fae70176db86848532debb2892796f
Manish Nonha <manish06598@gmail.com>
2025-09-28 09:45:57 +0530

    fixing session error

--- app.js
+++ app.js
@@ -237,7 +237,7 @@ const adminSession = session({
   rolling: true,
   cookie: {
     httpOnly: true,
-    secure: true,
+    secure: false,
     sameSite: "lax",
     maxAge: 10 * 24 * 60 * 60 * 1000,
   },
